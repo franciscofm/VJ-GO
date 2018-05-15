4b532df6e92afa64f0a4faa6067059622e13eebc
Fran <francisco.fernandez.manyas@gmail.com>
2018-05-15 17:10:30 +0200

    Level finished, added simple enemy movement and some Directories reorder

--- Assets/Level/Enemy.cs
+++ Assets/Level/Enemy.cs
@@ -3,5 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : Entity {
-	
+
+	public List<Spot> Route;
+
+	int currentSpot = -1;
+	public virtual void IA() {
+		Debug.Log ("Hello");
+		if (actionsLeft <= 0) return;
+		if (currentSpot < 0) currentSpot = Route.IndexOf (spot);
+		Move (this, Route [(currentSpot + 1) % Route.Count].transform.position + Vector3.up, 0.2f, Level.instance.curveMovement, delegate {
+			++currentSpot;
+			Level.instance.EndActionEnemy (this);
+		});
+		--actionsLeft;
+	}
 }
